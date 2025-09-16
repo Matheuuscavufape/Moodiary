93af550276f02aaca021fe792f2d794a69ded843
Matheus Cavalcante <matheus.mcavalcante@ufape.edu.br>
2025-09-16 17:25:58 -0300

    fix: pagina do frontend branca

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';  
 import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf], 
   template: `
-  <header>
-    <div><strong>Diário Pessoal</strong></div>
-    <nav *ngIf="auth.isLoggedIn()">
-      <a routerLink="/diary">Entradas</a>
-      <a routerLink="/mood">Humor</a>
-      <button class="btn" (click)="logout()">Sair</button>
-    </nav>
-  </header>
-  <div class="container">
-    <router-outlet></router-outlet>
-  </div>
+    <header>
+      <div><strong>Diário Pessoal</strong></div>
+      <nav *ngIf="auth.isLoggedIn()">
+        <a routerLink="/diary">Entradas</a>
+        <a routerLink="/mood">Humor</a>
+        <button class="btn" (click)="logout()">Sair</button>
+      </nav>
+    </header>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
   `
 })
 export class AppComponent {
