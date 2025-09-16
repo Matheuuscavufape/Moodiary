e81598a5ea06fa858af13298db119351a030727c
Matheus Cavalcante <matheus.mcavalcante@ufape.edu.br>
2025-09-16 19:03:24 -0300

    fix: resolvendo erro integração backend com frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://moodiary-umse.onrender.com/api'  // backend no Render
+  apiUrl: 'https://moodiary-umse.onrender.com' 
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'  // backend local
+  apiUrl: 'http://localhost:8080'  // backend local
 };
